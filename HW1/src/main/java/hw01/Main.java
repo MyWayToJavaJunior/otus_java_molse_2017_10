3aa502facb46e4dc345a586ff4ba7d86b00197c7
molse <smolokhov@gmail.com>
2017-10-14 23:48:54 +0300

    HW1 - obfuscation - исправление замечаний.

--- HW1/src/main/java/hw01/Main.java
+++ HW1/src/main/java/hw01/Main.java
@@ -1,14 +1,13 @@
 package hw01;
 
 import com.google.common.collect.Lists;
-import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String... args) {
-        MySampleObfuscatorClass mySampleObfuscatorClass = new MySampleObfuscatorClass(IntStream.rangeClosed(1, 50).mapToObj(Integer::new).collect(Collectors.toList()));
+        MySampleObfuscatorClass mySampleObfuscatorClass = new MySampleObfuscatorClass(IntStream.rangeClosed(1, 50).boxed().collect(Collectors.toList()));
         Lists.partition(mySampleObfuscatorClass.getIntegers(), 10).forEach(System.out::println);
     }
 }
